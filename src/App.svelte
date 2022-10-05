daff01d18eba0cdf9e77ab35e8897a4aa566e66c
sooraj <sooraj@mozilor.com>
2022-10-05 10:31:05 +0530

    login

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,52 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import { Router, Route, navigate } from "svelte-routing";
+  import { onMount } from "svelte";
+  import SignUp from "./pages/SignUp.svelte";
+  import Login from "./pages/Login.svelte";
+  import { user } from "./store/index";
+  import api from "./api/index";
+
+
+  export let url = "";
+  let isLoading = true;
+
+ async function fetchAccount(){
+    const account = await api.getAccount();
+    user.update(() => account);
+  }
+
+
+  onMount(async () => {
+    console.log("user", $user)
+    if(!$user){
+      fetchAccount()
+    }
+    else{
+      navigate("/login");
+    }
+  });
+
+  isLoading = false;
+
+  $: $user && console.log("user",$user)
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
 
-  <Counter />
+  <Router {url}>
+    <div class="container-fluid px-0">
+      {#if isLoading}
+        <div class="row min-vh-100 justify-content-center align-items-center">
+          <div class="spinner-border text-center" role="status" />
+        </div>
+      {:else}
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+          <Route path="/"  >
+            hello
+          </Route>
+      {/if}
+    </div>
+  </Router>
 
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
